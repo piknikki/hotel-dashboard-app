fd748b3d1642b5c1773a5c0c61819aeeb1257f6b
Nikki <piknik@me.com>
2021-03-04 15:15:34 -0700

    Added some fetch biz now probably use promise all

--- src/User.js
+++ src/User.js
@@ -1,9 +1,13 @@
 class User {
-  constructor(newUser) {
+  constructor(newUser, bookings) {
     // gets passed in when fetch gets it from api call
     this.id = newUser.id
     this.name = newUser.name
+    this.allBookings = bookings //array of bookings where newUser.id === booking.userID
+  }
 
+  findPastBookings() {
+    this.allBookings.filter()
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -4,31 +4,65 @@ import './images/pouncecat_orange.png';
 import './images/pouncecat_white.png';
 
 import User from "./User";
-
+import Booking from "./Booking";
 
 // *** Global variables *** //
 // user will input the string of their username - either customer or manager
 // use a query selector to get the input value of the name
+const userNameSelector = document.querySelector('.user-name')
+
 const userName = 'customer2' // todo => this will come from the login input not sure where it will live yet
 const userId = userName.slice(8);
 
 // *** Build page *** //
 // fetch calls
 
-
+/// GET user info
 fetch(`http://localhost:3001/api/v1/customers/${userId}`)
-  .then(response => response.json()) // todo ==> add error handling
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json()
+  })
   .then(data => {
     createNewUser(data)
   })
+  .catch(error => console.log(error.message))
+
+/// GET booking info
+fetch(`http://localhost:3001/api/v1/bookings`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json()
+  })
+  .then(data => {
+    createBookingsData(data)
+  })
+  .catch(error => console.log(error.message))
 
 const createNewUser = (data) => {
-  const date = new Date();
+  // const date = new Date();
+  // const today = date.toLocaleDateString();
+  const date = '2019/01/01';
   const user = new User(data)
-  const today = date.toLocaleDateString();
+  console.log(user)
 
-  document.querySelector('.date').innerHTML = `${today}`
-  document.querySelector('.user-name').innerHTML = `Hi, ${user.name}`
+  document.querySelector('.date').innerHTML = `${date}`
+  userNameSelector.setAttribute('data-userId', user.id)
+  userNameSelector.innerHTML = `Hi, ${user.name}`
+}
+
+const createBookingsData = (data) => {
+  const currentUserId = userNameSelector.getAttribute('data-userId')
+
+  const currentUserBookings = data.filter(booking => {
+    return booking.userID === currentUserId
+  })
+
+  return currentUserBookings
 }
 
 // *** General Functions *** //
